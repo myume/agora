982167692c90217bd955742160e0eb3d1348b743
myu <contact@memyu.com>
2025-11-25 11:42:42 -0500

    feat: change default listening address to allow outside connections

--- agora-proxy/src/main.rs
+++ agora-proxy/src/main.rs
@@ -1,15 +1,21 @@
-use agora_proxy::server::{Server, ServerConfig};
+use agora_proxy::server::{ProxyEntry, Server, ServerConfig};
+use regex::Regex;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
-    let addr = "127.0.0.1:8080";
-    let config = ServerConfig::default();
+    let addr = "0.0.0.0:8080";
+    let mut config = ServerConfig::default();
+    config.reverse_proxy_mapping.push((
+        Regex::new(".*").unwrap(),
+        ProxyEntry {
+            addr: "127.0.0.1:3000".to_string(),
+        },
+    ));
     // TODO: figure out how to allow user to set up proxy
 
     let server = Server::new(config);
-
     server.listen(addr).await?;
 
     Ok(())
