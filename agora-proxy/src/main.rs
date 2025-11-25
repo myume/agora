768b29e94c95864067f10861051f5d540f36c522
myu <contact@memyu.com>
2025-11-25 14:03:52 -0500

    feat: add cli

--- agora-proxy/src/main.rs
+++ agora-proxy/src/main.rs
@@ -1,11 +1,37 @@
 use agora_proxy::server::{ProxyEntry, Server, ServerConfig};
+use clap::{Parser, Subcommand};
 use regex::Regex;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Start the server
+    Start {
+        #[arg(short, long, default_value_t = 8080)]
+        /// The port the server should listen on
+        port: u16,
+    },
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Start { port } => run(port).await,
+    }
+}
+
+async fn run(port: u16) -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
-    let addr = "0.0.0.0:8080";
+    let addr = format!("0.0.0.0:{}", port);
     let mut config = ServerConfig::default();
     config.reverse_proxy_mapping.push((
         Regex::new(".*").unwrap(),
@@ -17,7 +43,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: figure out how to allow user to set up proxy
 
     let server = Server::new(config);
-    server.listen(addr).await?;
+    server.listen(&addr).await?;
 
     Ok(())
 }
